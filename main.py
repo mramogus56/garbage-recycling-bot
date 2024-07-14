cf711e574d949d6a65659cea955dc1aa7e0ed43e
mramogus56 <ponomaryovwsewolod@yandex.ru>
2024-07-14 14:24:18 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+@bot.command()
+async def helping(ctx):
+    await ctx.send('Команды: ?choice, ?recycling, Другие команды в разработке...')
+
+@bot.command()
+async def choice(ctx):
+    choises = ('Выбросить мусор', 'Поиграть в Комп', 'Сдать бумагу на переработку')  
+    await ctx.send(random.choice(choises))
+
+@bot.command()
+async def recycling(ctx):
+    await ctx.send('Если вы хотите переработать мусор, достаточно сдать его в пункт, где принимают разные материалы. Статья о переработке мусора: https://multifoto.ru/blog-lifestyle/sortirovka-musora-dlya-detey-kak-privit-privychku-detyam-berech-prirodu/')
+ 
+
+ 
+
+ 
+
+bot.run('token')
