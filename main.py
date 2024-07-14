f38146477b26268fc207a2c55fd632c03feb0bef
mramogus56 <ponomaryovwsewolod@yandex.ru>
2024-07-14 14:54:34 +0600

    Update main.py
    Добавлена новая функция ?advice

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ bot = commands.Bot(command_prefix='?', intents=intents)
 
 @bot.command()
 async def helping(ctx):
-    await ctx.send('Команды: ?choice, ?recycling, Другие команды в разработке...')
+    await ctx.send('Команды: ?choice, ?recycling, ?advice, Другие команды в разработке...')
 
 @bot.command()
 async def choice(ctx):
@@ -19,6 +19,10 @@ async def choice(ctx):
 @bot.command()
 async def recycling(ctx):
     await ctx.send('Если вы хотите переработать мусор, достаточно сдать его в пункт, где принимают разные материалы. Статья о переработке мусора: https://multifoto.ru/blog-lifestyle/sortirovka-musora-dlya-detey-kak-privit-privychku-detyam-berech-prirodu/')
+
+@bot.command()
+async def advice(ctx):
+    await ctx.send('Совет по переработке бумаги: Собери ненужную мукулатуру по всему дому и сдай. Итог: и заработаешь немного, и чуть природу спасёшь).')
  
 
  
